cd9b69c76c3e9d58c43ceef0179930bedbc9b852
nagendra <nagendrarawat63@gmail.com>
2021-08-25 22:45:43 +0530

    created components and minor changes to index.ejs

--- src/server.js
+++ src/server.js
@@ -1,11 +1,20 @@
 const express = require("express")
 const path = require("path");
+
+const dotenv = require('dotenv');
+
+
 const app = express();
 app.use(express.json());
 const connect = require("./configs/db");
 app.use(express.static(path.join(__dirname, 'public')));
 
-const PORT = process.env.PORT || 2312;
+// const morgan = require('morgan');
+
+
+dotenv.config({path: 'config.env'});
+
+const PORT = process.env.PORT || 8080;
 
 
 app.set("views", path.join(__dirname), "views");
@@ -18,6 +27,9 @@ const user_controller = require("./controllers/user.controller");
 
 app.use("/user", user_controller);
 
+app.get('/', (req,res)=>{
+    res.render("views/index")
+})
 
 app.listen(PORT, async()=>{
     await connect();
